21929d1ecea25d452b37f35350b5b42682dcc7c7
Oleksandr Kuvshynov <oleksandr@oleksandrs-air.mynetworksettings.com>
2023-02-09 11:59:21 -0500

    cubestat: several changes
    1. bytes -> kbytes
    2. larger buffer
    3. allow read from last snapshot
    4. add numeric value
    5. better autoscale

--- cubestat.py
+++ cubestat.py
@@ -13,86 +13,86 @@ def get_cells():
     res.append(f'{bg[-1]}{fg[0]} {rst}')
     return res
 
-def clamp(v, a, b):
-    return max(a, min(v, b))
-
 def horizon_line(series, domain, cells):
+    if not series:
+        return ''
     range = len(cells)
-    (va, vb) = domain
-    if vb is None:
-        vb = max(series)
-    if va == vb:
-        vb = va + 1
-    cell = lambda v: cells[clamp(int((v - va) * range / (vb - va)), 0, range - 1)]
-    return ''.join([cell(v) for v in series])
+    (a, b) = domain
+    if b is None:
+        b = max(series)
+    if a == b:
+        b = a + 1
+    clamp = lambda v, a, b: max(a, min(v, b))
+    cell = lambda v: cells[clamp(int((v - a) * range / (b - a)), 0, range - 1)]
+    return ''.join([cell(v) for v in series]) + f' {series[-1]:.3f}'
 
 def collect_metrics(m):
-    # add disk and network
     res = {}
     res['gpu util %'] = 1.0 - m['gpu']['idle_ratio']
     res['ane_energy'] = m['processor']['ane_energy']
-    res['nw i bytes/s'] = m['network']['ibyte_rate']
-    res['nw o bytes/s'] = m['network']['obyte_rate']
-    res['disk r bytes/s'] = m['disk']['rbytes_per_s']
-    res['disk w bytes/s'] = m['disk']['wbytes_per_s']
+    res['nw i kbytes/s'] = m['network']['ibyte_rate'] / 1024.0
+    res['nw o kbytes/s'] = m['network']['obyte_rate'] / 1024.0
+    res['disk r kbytes/s'] = m['disk']['rbytes_per_s'] / 1024.0
+    res['disk w kbytes/s'] = m['disk']['wbytes_per_s'] / 1024.0
 
     for cluster in m['processor']['clusters']:
-        name = cluster['name']
         for cpu in cluster['cpus']:
-            res[f'{name} cpu {cpu["cpu"]} util %'] = 1.0 - cpu['idle_ratio']
+            res[f'{cluster["name"]} cpu {cpu["cpu"]} util %'] = 1.0 - cpu['idle_ratio']
 
     return res
 
-auto_domains = ['ane_energy', 'nw i bytes/s', 'nw o bytes/s', 'disk r bytes/s', 'disk w bytes/s']
+auto_domains = ['ane_energy', 'nw i kbytes/s', 'nw o kbytes/s', 'disk r kbytes/s', 'disk w kbytes/s']
+all_cells = get_cells()
+cubes = {}
+width = 120
 
-def append_data(data, new_point):
+def append_data(new_point):
     for k, v in new_point.items():
-        if k not in data.keys():
-            data[k] = [v]
+        if k not in cubes.keys():
+            cubes[k] = [v]
         else:
-            data[k].append(v)
-            if (len(data[k]) > 80):
-                data[k] = data[k][1:]
+            cubes[k].append(v)
+            if (len(cubes[k]) > width):
+                cubes[k] = cubes[k][1:]
+
+def render():
+    print('-' * width)
+    for k, v in cubes.items():
+        print(k)
+        domain = (0.0, None) if k in auto_domains else (0.0, 1.0)
+        print(horizon_line(v, domain, all_cells))
 
 def start():
-    cmd = ['sudo', 'powermetrics', '-f', 'plist', '-o', '/tmp/cubestat', '-i', '1000']
+    cmd = ['sudo', 'powermetrics', '-f', 'plist', '-o', '/tmp/cubestat', '-i', '5000']
     try:
         os.remove('/tmp/cubestat')
     except FileNotFoundError:
         pass
 
-    cubes = {}
-    all_cells = get_cells()
     subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     while True:
         try:
             curr = bytearray()
             with open('/tmp/cubestat', 'rb') as f:
                 while True:
-                    pos = f.tell()
-                    data = f.read(1024)
+                    data = f.read(2 ** 20)
+                    curr.extend(data)
+                    if b'</plist>\n' in curr:
+                        readings = bytes(curr).split(b'\x00')
+                        if not readings[-1].endswith(b'</plist>\n'):
+                            curr = bytearray(readings.pop())
+                        else:
+                            curr = bytearray()
 
-                    if not data:
-                        time.sleep(0.1)
-                        f.seek(pos)
+                        for r in readings:
+                            if r:
+                                append_data(collect_metrics(plistlib.loads(r)))
+                        render()
                     else:
-                        curr.extend(data)
-
-                        bcurr = bytes(curr)
-                        if b'\x00' in bcurr:                        
-                            readings = bcurr.split(b'\x00')
-                            for r in readings[:-1]:
-                                append_data(cubes, collect_metrics(plistlib.loads(r)))
-                            
-                            curr = bytearray(readings[-1])
-
-                            for k, v in cubes.items():
-                                print(k)
-                                domain = (0.0, None) if k in auto_domains else (0.0, 1.0)
-                                print(horizon_line(v, domain, all_cells))
+                        time.sleep(0.05)
 
         except FileNotFoundError:
-            time.sleep(0.1)
+            time.sleep(0.05)
 
 if __name__ == '__main__':
     start()
