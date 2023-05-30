09b58393736db2c8ca8b622c2d641e10eb08f2cc
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-05-30 10:41:18 -0400

    cubestat: better formating + perf improvement

--- cubestat.py
+++ cubestat.py
@@ -5,14 +5,20 @@ import subprocess
 import curses
 import argparse
 import collections
+import itertools
+import sys
+import logging
+
+logging.basicConfig(filename='/tmp/cubestat.log')
 
 parser = argparse.ArgumentParser("cubestate monitoring")
-parser.add_argument('--refresh_ms', '-i', type=int, default=1000)
-parser.add_argument('--width', '-w', type=int, default=80)
+parser.add_argument('--refresh_ms', '-i', type=int, default=500)
+parser.add_argument('--buffer_size', type=int, default=500)
+
 args = parser.parse_args()
 
 auto_domains = ['nw i kbytes/s', 'nw o kbytes/s', 'disk r kbytes/s', 'disk w kbytes/s']
-cubes = collections.defaultdict(lambda: collections.deque(maxlen=args.width))
+cubes = collections.defaultdict(lambda: collections.deque(maxlen=args.buffer_size))
 
 def gen_cells():
     chrs = [' ', '▁', '▂', '▃', '▄', '▅', '▆', '▇', '█']
@@ -30,27 +36,37 @@ def process_snapshot(m):
     cubes['nw o kbytes/s'].append(m['network']['obyte_rate'] / 1024.0)
     cubes['disk r kbytes/s'].append(m['disk']['rbytes_per_s'] / 1024.0)
     cubes['disk w kbytes/s'].append(m['disk']['wbytes_per_s'] / 1024.0)
-
     for cluster in m['processor']['clusters']:
         for cpu in cluster['cpus']:
             cubes[f'{cluster["name"]} cpu {cpu["cpu"]} util %'].append(100.0 - 100.0 * cpu['idle_ratio'])
 
 def render(stdscr, cells):
-    stdscr.clear()
+    stdscr.erase()
+    rows, cols = stdscr.getmaxyx()
+    spacing_width = 1
+    spacing = ' ' * spacing_width
     range = len(cells)
-    for i, (k, series) in enumerate(cubes.items()):
-        stdscr.addstr(i * 2, 0, '╔ ' + k)
+    for i, (title, series) in enumerate(cubes.items()):
+        if rows <= i * 2 + 1 or cols <= 3:
+            break
+
+        stdscr.addstr(i * 2, 0, '╔' + spacing + title)
         stdscr.addstr(i * 2 + 1, 0, '╚')
         
-        b = max(1, max(series)) if k in auto_domains else 100.0
+        strvalue = f'{series[-1]:.1f}{spacing}╗'
+        stdscr.addstr(i * 2, cols - len(strvalue), strvalue)
+        stdscr.addstr(i * 2 + 1, cols - spacing_width - 1, f'{spacing}╝')
+
+        index = max(0, len(series) - (cols - 2 * spacing_width))
+        data_slice = list(itertools.islice(series, index, None))
+        b = max(1, max(data_slice)) if title in auto_domains else 100.0
+
         clamp = lambda v, a, b: max(a, min(v, b))
         cell = lambda v: cells[clamp(int(v * range / b), 0, range - 1)]
-        for j, v in enumerate(series):
-            chr, color_pair = cell(v)
-            stdscr.addstr(i * 2 + 1, 2 + j, chr, curses.color_pair(color_pair))
         
-        stdscr.addstr(i * 2 + 1, 2 + len(series), f' {series[-1]:.1f}')
-
+        for j, v in enumerate(data_slice):
+            chr, color_pair = cell(v)
+            stdscr.addstr(i * 2 + 1, j + 1 + spacing_width, chr, curses.color_pair(color_pair))
     stdscr.refresh()
 
 def main(stdscr):
@@ -65,8 +81,9 @@ def main(stdscr):
     cells = gen_cells()
 
     while True:
-        buf.extend(p.stdout.readline())
-        if b'</plist>\n' in buf:
+        line = p.stdout.readline()
+        buf.extend(line)
+        if b'</plist>\n' == line:
             snapshots = bytes(buf).strip(b'\x00').split(b'\x00')
             if not snapshots[-1].endswith(b'</plist>\n'):
                 buf = bytearray(snapshots.pop())
