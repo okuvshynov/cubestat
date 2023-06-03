8b330dd3895970d28cba6fe9e94ffb476d36788b
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-06-03 12:03:35 -0400

    cubestat: allow switching cpu visualization mode

--- cubestat.py
+++ cubestat.py
@@ -23,12 +23,16 @@ class Percentages(Enum):
         return Percentages.hidden if self == Percentages.last else Percentages.last
 
 class CPUMode(Enum):
-    collapsed = 'collapsed'
-    expanded = 'expanded'
-    cluster = 'cluster'
+    all = 'all'
+    by_cluster = 'by_cluster'
+    by_core = 'by_core'
 
     def __str__(self):
         return self.value
+    
+    def next(self):
+        values = list(CPUMode)
+        return values[(values.index(self) + 1) % len(values)]
     
 class Color(Enum):
     red = 'red'
@@ -42,7 +46,7 @@ class Color(Enum):
 parser = argparse.ArgumentParser("./cubestat.py")
 parser.add_argument('--refresh_ms', '-i', type=int, default=500, help='This argument is passed to powermetrics as -i')
 parser.add_argument('--buffer_size', type=int, default=500, help='How many datapoints to store. Having it larger than screen width is a good idea as terminal window can be resized')
-parser.add_argument('--cpu', type=CPUMode, default=CPUMode.expanded, choices=list(CPUMode))
+parser.add_argument('--cpu', type=CPUMode, default=CPUMode.by_core, choices=list(CPUMode), help='CPU mode - showing all cores, only cumulative by cluster or both. Can be toggled by pressing c.')
 parser.add_argument('--color', type=Color, default=Color.mixed, choices=list(Color))
 parser.add_argument('--percentages', type=Percentages, default=Percentages.hidden, choices=list(Percentages), help='Show numeric utilization percentage. Also can be toggled by pressing p.')
 args = parser.parse_args()
@@ -61,6 +65,9 @@ colormap = {}
 snapshots_observed = 0
 snapshots_rendered = 0
 percentage_mode = args.percentages
+cpu_cubes = []
+cpu_cluster_cubes = []
+cpumode = args.cpu
 
 colorschemes = {
     Color.green: [-1, 150, 107, 22],
@@ -81,32 +88,28 @@ def gen_cells():
     return cells
 
 def process_snapshot(m):
-    global snapshots_rendered, snapshots_observed
+    global snapshots_rendered, snapshots_observed, cpu_cluster_cubes, cpu_cubes
     initcolormap = not colormap
-    
-    idle, total = 0.0, 0.0
     
     with cubelock:
         for cluster in m['processor']['clusters']:
+            idle_cluster, total_cluster = 0.0, 0.0
+            cluster_title = f'{cluster["name"]} total CPU util'
+            if not cluster_title in cubes:
+                cubes[cluster_title] = collections.deque(maxlen=args.buffer_size)
+                cpu_cluster_cubes.append(cluster_title)
+                colormap[cluster_title] = cpu_color
             for cpu in cluster['cpus']:
-                if args.cpu == CPUMode.expanded:
-                    cubes[f'{cluster["name"]} CPU {cpu["cpu"]} util'].append(100.0 - 100.0 * cpu['idle_ratio'])
-                    if initcolormap:
-                        colormap[f'{cluster["name"]} CPU {cpu["cpu"]} util'] = cpu_color
-                else:
-                    idle += cpu['idle_ratio']
-                    total += 1.0
-            if args.cpu == CPUMode.cluster:
-                cubes[f'{cluster["name"]} total CPU util'].append(100.0 - 100.0 * idle / total)
+                title = f'{cluster["name"]} CPU {cpu["cpu"]} util'
+                cubes[title].append(100.0 - 100.0 * cpu['idle_ratio'])
                 if initcolormap:
-                    colormap[f'{cluster["name"]} total CPU util'] = cpu_color
-                idle, total = 0.0, 0.0
+                    cpu_cubes.append(title)
+                    colormap[title] = cpu_color
+                idle_cluster += cpu['idle_ratio']
+                total_cluster += 1.0
+            
+            cubes[cluster_title].append(100.0 - 100.0 * idle_cluster / total_cluster)
                 
-            if args.cpu == CPUMode.collapsed:
-                cubes[f'total CPU util'].append(100.0 - 100.0 * idle / total)
-                if initcolormap:
-                    colormap[f'total CPU util'] = cpu_color
-
         cubes['GPU util'].append(100.0 - 100.0 * m['gpu']['idle_ratio'])
         ane_scaling = 8.0 * args.refresh_ms
         cubes['ANE util'].append(100.0 * m['processor']['ane_energy'] / ane_scaling)
@@ -115,9 +118,8 @@ def process_snapshot(m):
             colormap['ANE util'] = ane_color
         snapshots_observed += 1
 
-
 def render(stdscr, cellsmap):
-    global snapshots_rendered, snapshots_observed
+    global snapshots_rendered, snapshots_observed, cpu_cluster_cubes, cpu_cubes
     with cubelock:
         if snapshots_observed == snapshots_rendered:
             return
@@ -125,8 +127,10 @@ def render(stdscr, cellsmap):
     rows, cols = stdscr.getmaxyx()
     spacing = ' ' * spacing_width
 
+    filter_cpu = lambda it : cpumode == CPUMode.all or (cpumode == CPUMode.by_cluster and it[0] not in cpu_cubes) or (cpumode == CPUMode.by_core and it[0] not in cpu_cluster_cubes)
+
     with cubelock:
-        for i, (title, series) in enumerate(cubes.items()):
+        for i, (title, series) in enumerate(filter(filter_cpu, cubes.items())):
             cells = cellsmap[colormap[title]]
             range = len(cells)
         
@@ -137,10 +141,7 @@ def render(stdscr, cellsmap):
             stdscr.addstr(i * 2, 0, titlestr)
             stdscr.addstr(i * 2 + 1, 0, '╚')
             
-            if percentage_mode == Percentages.last:
-                strvalue = f'last:{series[-1]:3.0f}%{spacing}╗'
-            else:
-                strvalue = f'{spacing}╗'
+            strvalue = f'last:{series[-1]:3.0f}%{spacing}╗' if percentage_mode == Percentages.last else f'{spacing}╗'
             stdscr.addstr(i * 2, cols - len(strvalue), strvalue)
             stdscr.addstr(i * 2 + 1, cols - spacing_width - 1, f'{spacing}╝')
 
@@ -160,7 +161,7 @@ def render(stdscr, cellsmap):
     stdscr.refresh()
 
 def main(stdscr, powermetrics, firstline=''):
-    global percentage_mode, cubelock
+    global percentage_mode, cubelock, cpumode
     stdscr.nodelay(False)
     stdscr.timeout(50)
     curses.curs_set(0)
@@ -190,7 +191,9 @@ def main(stdscr, powermetrics, firstline=''):
         if key == ord('p'):
             with cubelock:
                 percentage_mode = percentage_mode.other()
-
+        if key == ord('c'):
+            with cubelock:
+                cpumode = cpumode.next()
 
 if __name__ == '__main__':
     cmd = ['sudo', 'powermetrics', '-f', 'plist', '-i', str(args.refresh_ms), '-s', 'cpu_power,gpu_power,ane_power']
