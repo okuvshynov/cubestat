93e44d30caca264d18b42c2cf7953a46540d7ff9
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-06-08 17:11:23 -0400

    cubestat: fix setup

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ setup(
     author='Oleksandr Kuvshynov',
     author_email='okuvshynov@gmail.com',
     description='Horizon chart in terminal for CPU/GPU/ANE monitoring for Apple M1/M2',
+    long_description='Horizon chart in terminal for CPU/GPU/ANE monitoring for Apple M1/M2',
     packages=find_packages(),
     url='https://github.com/okuvshynov/cubestat',
     entry_points={
         'console_scripts': [
             'cubestat = cubestat.cubestat:main'
         ]
-    }
+    },
 )
