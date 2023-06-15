ffdbfb28f1e3f890cc316df3e66f414e1a07be81
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-06-15 19:47:25 -0400

    cubestat: 0.0.4 -- setup deps

--- setup.py
+++ setup.py
@@ -2,13 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='cubestat',
-    version='0.0.3',
+    version='0.0.4',
     author='Oleksandr Kuvshynov',
     author_email='okuvshynov@gmail.com',
     description='Horizon chart in terminal for CPU/GPU/ANE monitoring',
     long_description='Horizon chart in terminal. Supports CPU/GPU/ANE monitoring for Apple M1/M2, nVidia GPUs',
     packages=find_packages(),
-    install_requires=['psutil>=5.9.5'],
+    install_requires=[
+        'psutil>=5.9.5',
+        'pynvml; platform_system=="Linux"'
+    ],
     url='https://github.com/okuvshynov/cubestat',
     entry_points={
         'console_scripts': [
