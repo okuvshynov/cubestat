c5c2c9875db6a7bf90e023646f1d0f567663103d
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2023-06-15 22:37:31 -0400

    cubestat: change app name

--- cubestat/cubestat.py
+++ cubestat/cubestat.py
@@ -39,7 +39,7 @@ class Color(EnumStr):
     blue = 'blue'
     mixed = 'mixed'
 
-parser = argparse.ArgumentParser("./cubestat.py")
+parser = argparse.ArgumentParser("cubestat")
 parser.add_argument('--refresh_ms', '-i', type=int, default=500, help='Update frequency, milliseconds')
 parser.add_argument('--buffer_size', type=int, default=500, help='How many datapoints to store. Having it larger than screen width is a good idea as terminal window can be resized')
 parser.add_argument('--cpu', type=CPUMode, default=CPUMode.all, choices=list(CPUMode), help='CPU mode - showing all cores, only cumulative by cluster or both. Can be toggled by pressing c.')
