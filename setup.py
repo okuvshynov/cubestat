335aa2638c9f4502a843c87bc5fd3a5d204f2c41
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-03-29 15:47:09 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='cubestat',
-    version='0.0.16',
+    version='0.1.0',
     author='Oleksandr Kuvshynov',
     author_email='okuvshynov@gmail.com',
     description='Horizon chart in terminal for system monitoring',
-    long_description='Horizon chart in terminal. Supports CPU/GPU/ANE/RAM/IO monitoring for Apple M1/M2, nVidia GPUs',
+    long_description='Horizon chart in terminal. Supports CPU/GPU/ANE/RAM/swap/IO monitoring for Apple M1/M2/M3, nVidia GPUs',
     packages=find_packages(),
     install_requires=[
         'psutil>=5.9.5',
