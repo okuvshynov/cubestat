7f8d127194c56efa9128af8ba3983ff562806c21
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-03-29 15:51:03 -0400

    bump to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cubestat',
-    version='0.1.2',
+    version='0.2.0',
     author='Oleksandr Kuvshynov',
     author_email='okuvshynov@gmail.com',
     description='Horizon chart in terminal for system monitoring',
