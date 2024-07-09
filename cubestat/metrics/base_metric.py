c8b1ff8edc504fc63bf7da0d3b1d5832ebd7eda6
Oleksandr Kuvshynov <661042+okuvshynov@users.noreply.github.com>
2024-07-09 11:42:11 -0400

    cubestat: abstract metric

--- cubestat/metrics/base_metric.py
+++ cubestat/metrics/base_metric.py
@@ -3,6 +3,9 @@ from abc import ABC, abstractmethod
 from cubestat.common import SimpleMode
 
 class base_metric(ABC):
+    ###########################################################################
+    # abstract methods each metric needs to implement
+    ###########################################################################
     @abstractmethod
     def read(self, context):
         pass
@@ -15,20 +18,30 @@ class base_metric(ABC):
     def format(self, values, idxs):
         pass
 
+    @classmethod
+    @abstractmethod
+    def key(cls):
+        pass
+
+    ###########################################################################
+    # methods with default implementation which each metric might override
+    ###########################################################################
+
     # configure metric instance
     def configure(self, config):
         self.mode = SimpleMode.show
         return self
+
+    # hotkey circle through the display modes
+    def hotkey(self):
+        return None
 
     # if we define any options to select/toggle view mode
     @classmethod
     def configure_argparse(cls, parser):
         pass
 
+    # help message to be used for this metric. 
     @classmethod
-    @abstractmethod
-    def key(cls):
-        pass
-
-    def hotkey(self):
+    def help(self):
         return None
